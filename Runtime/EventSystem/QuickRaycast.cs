3826212cecf7121965fd1ccabaa408296293dbd0
James Kim <eodnjs2998@gmail.com>
2023-06-16 09:34:47 +0900

    QuickRaycast 에서 GetRaycastableGraphicsForCanvas 를 사용하도록 변경.

--- Runtime/EventSystem/QuickRaycast.cs
+++ Runtime/EventSystem/QuickRaycast.cs
@@ -100,7 +100,7 @@ namespace UnityEngine.EventSystems
             out RaycastResult raycastResult)
         {
             Assert.AreNotEqual(RenderMode.ScreenSpaceOverlay, canvas.renderMode);
-            var graphics = GraphicRegistry.GetGraphicsForCanvas(canvas);
+            var graphics = GraphicRegistry.GetRaycastableGraphicsForCanvas(canvas);
             if (graphics == null || graphics.Count == 0)
             {
                 raycastResult = default;
