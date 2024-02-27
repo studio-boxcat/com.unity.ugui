d70dec1adcb1d87295bd58d364f509be4ee77cf6
James Kim <eodnjs2998@gmail.com>
2024-02-27 18:58:52 +0900

    improve logging.

--- Runtime/UI/Core/Utility/TextGeneratorPool.cs
+++ Runtime/UI/Core/Utility/TextGeneratorPool.cs
@@ -8,17 +8,21 @@ namespace UnityEngine.UI
 
         public static TextGenerator Rent()
         {
-#if UNITY_EDITOR
-            if (_pool.Count > 100)
-                Debug.LogError("TextGeneratorPool is too big. Something is wrong.");
-#endif
-
             return _pool.TryPop(out var instance)
                 ? instance : new TextGenerator();
         }
 
         public static void Return(TextGenerator instance)
         {
+            if (_pool.Count > 100)
+            {
+#if DEBUG
+                Debug.LogWarning("TextGeneratorPool is too big. Something is wrong.");
+#endif
+                return;
+            }
+
+
             // Reset m_CachedXXX fields to false.
             instance.PopulateWithErrors(null, default, null);
             // Make sure next time we use it, it will be re-populated.
