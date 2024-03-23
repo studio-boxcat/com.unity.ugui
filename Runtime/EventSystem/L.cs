f6506982a7e377425890d7c0b087f6f1b5c965bb
James Kim <eodnjs2998@gmail.com>
2024-03-23 18:45:54 +0900

    wip

--- Runtime/EventSystem/L.cs
+++ Runtime/EventSystem/L.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 
 namespace UnityEngine.EventSystems
 {
-    public static class L
+    static class L
     {
         [Conditional("DEBUG")]
         public static void W(string message, Object context = null)
