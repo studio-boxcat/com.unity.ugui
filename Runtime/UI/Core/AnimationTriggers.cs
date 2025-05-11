dfbfe427ec1eaaa50dbd90285d68e06afa68e753
James Kim <eodnjs2998@gmail.com>
2025-05-11 17:07:57 +0900

    migrated AnimationTriggers

--- Runtime/UI/Core/AnimationTriggers.cs
+++
@@ -1,8 +0,0 @@
-namespace UnityEngine.UI
-{
-    public static class AnimationTriggers
-    {
-        public static readonly int Normal = Animator.StringToHash("Normal");
-        public static readonly int Pressed = Animator.StringToHash("Pressed");
-    }
-}
