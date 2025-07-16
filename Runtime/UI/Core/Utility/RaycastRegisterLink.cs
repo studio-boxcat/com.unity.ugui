284ce3dd3240608349340337bb5f23b015703503
James Kim (Daewon) <eodnjs2998@gmail.com>
2025-07-16 10:51:47 +0900

    simplified lang tracker, cleanup

--- Runtime/UI/Core/Utility/RaycastRegisterLink.cs
+++ Runtime/UI/Core/Utility/RaycastRegisterLink.cs
@@ -1,21 +1,20 @@
-using JetBrains.Annotations;
-
+#nullable enable
 namespace UnityEngine.UI
 {
     public struct RaycastRegisterLink
     {
-        [CanBeNull] Canvas _canvas;
+        private Canvas? _canvas;
 
-        public void Reset([CanBeNull] Canvas canvas, Graphic graphic)
+        public void Reset(Canvas? canvas, Graphic graphic)
         {
-            if (ReferenceEquals(_canvas, canvas))
+            if (_canvas.RefEq(canvas)) // no need to unregister & register again.
             {
                 if (_canvas is null)
                     return;
 
                 // If the graphic originally was raycastable, but now is not, we need to unregister it.
-                var raycastable = graphic.raycastTarget && graphic.isActiveAndEnabled;
-                if (raycastable == false)
+                var raycastable = graphic is { raycastTarget: true, isActiveAndEnabled: true };
+                if (raycastable is false)
                 {
                     GraphicRegistry.UnregisterRaycastGraphicForCanvas(_canvas, graphic);
                     _canvas = null;
@@ -29,7 +28,7 @@ namespace UnityEngine.UI
                 _canvas = null;
             }
 
-            if (canvas is not null && graphic.raycastTarget && graphic.isActiveAndEnabled)
+            if (canvas is not null && graphic is { raycastTarget: true, isActiveAndEnabled: true })
             {
                 GraphicRegistry.RegisterRaycastGraphicForCanvas(canvas, graphic);
                 _canvas = canvas;
