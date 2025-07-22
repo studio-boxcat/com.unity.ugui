a86fd13986589895eec2f6d207100f29b60af204
James Kim (Daewon) <eodnjs2998@gmail.com>
2025-07-23 03:31:31 +0900

    wip

---
+++ Runtime/Extras/NonDrawingGraphic.cs
@@ -0,0 +1,13 @@
+﻿namespace UnityEngine.UI
+{
+    /// A concrete subclass of the Unity UI `Graphic` class that just skips drawing.
+    /// Useful for providing a raycast target without actually drawing anything.
+    [RequireComponent(typeof(CanvasRenderer))]
+    [GraphicPropertyHide(GraphicPropertyFlag.Color | GraphicPropertyFlag.Material)]
+    public class NonDrawingGraphic : Graphic
+    {
+        public override void SetMaterialDirty() { }
+        public override void SetVerticesDirty() { }
+        protected override void UpdateGeometry() => canvasRenderer.Clear();
+    }
+}
