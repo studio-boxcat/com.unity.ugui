5fd5af25e6bf143a0f9f5340aa4a406f42c30543
James Kim (Daewon) <eodnjs2998@gmail.com>
2025-07-26 14:56:43 +0900

    validation for NonDrawingGraphic.cs

--- Runtime/Extras/NonDrawingGraphic.cs
+++ Runtime/Extras/NonDrawingGraphic.cs
@@ -1,13 +1,27 @@
-﻿namespace UnityEngine.UI
+﻿using Sirenix.OdinInspector;
+
+namespace UnityEngine.UI
 {
     /// A concrete subclass of the Unity UI `Graphic` class that just skips drawing.
     /// Useful for providing a raycast target without actually drawing anything.
     [RequireComponent(typeof(CanvasRenderer))]
     [GraphicPropertyHide(GraphicPropertyFlag.Color | GraphicPropertyFlag.Material)]
     public class NonDrawingGraphic : Graphic
+#if UNITY_EDITOR
+        , ISelfValidator
+#endif
     {
         public override void SetMaterialDirty() { }
         public override void SetVerticesDirty() { }
         protected override void UpdateGeometry() => canvasRenderer.Clear();
+
+#if UNITY_EDITOR
+        void ISelfValidator.Validate(SelfValidationResult result)
+        {
+            var (w, h) = rectTransform.rect.size;
+            if (w.Approximately(0) || h.Approximately(0))
+                result.AddError("NonDrawingGraphic must have a non-zero width and height.");
+        }
+#endif
     }
 }
