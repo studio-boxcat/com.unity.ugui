b3fa0b4ec36d7b0eef7cec62980e7168be228640
James Kim (Daewon) <eodnjs2998@gmail.com>
2025-07-28 18:17:53 +0900

    wip

---
+++ Runtime/UI/Core/Clipping/TargetClipper.cs
@@ -0,0 +1,64 @@
+// ReSharper disable InconsistentNaming
+
+#nullable enable
+using System;
+using Sirenix.OdinInspector;
+
+namespace UnityEngine.UI
+{
+    [ExecuteAlways]
+    public sealed class TargetClipper : UIBehaviour
+#if UNITY_EDITOR
+        , ISelfValidator
+#endif
+    {
+        [SerializeField, Required, RequiredListLength(MinLength = 1)]
+        private Graphic[] _targets = null!;
+
+        [SerializeField] private Vector4 _padding;
+        public Vector4 padding
+        {
+            get => _padding;
+            set => _padding = value;
+        }
+
+        [SerializeField] private Vector2Int _softness;
+        public Vector2Int softness
+        {
+            get => _softness;
+            set => _softness = value;
+        }
+
+        [NonSerialized] private RectTransform? _rectTransform;
+        public RectTransform rectTransform => _rectTransform ??= GetComponent<RectTransform>();
+
+        private void Update()
+        {
+            var canvas = _targets[0].canvas;
+            var clipRect = CanvasUtils.BoundingRect(
+                rectTransform, canvas, _padding, out var validRect);
+
+            foreach (var target in _targets)
+            {
+                var g = target;
+                g.SetClipSoftness(_softness);
+                g.SetClipRect(clipRect, validRect);
+            }
+        }
+
+#if UNITY_EDITOR
+        void ISelfValidator.Validate(SelfValidationResult result)
+        {
+            foreach (var graphic in _targets)
+            {
+                if (!graphic) continue;
+                // prevent the target graphic is added to the other clipper by ClipperRegistry.
+                if (graphic is MaskableGraphic { maskable: true })
+                    result.AddError("TargetClipper cannot be used with MaskableGraphic components.");
+                if (graphic.HasComponent<Clippable>())
+                    result.AddError("TargetClipper cannot be used with Clippable components.");
+            }
+        }
+#endif
+    }
+}
