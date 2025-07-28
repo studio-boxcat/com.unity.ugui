ce65c63cc9f55b4acc31a4dc02568f3ee7347779
James Kim (Daewon) <eodnjs2998@gmail.com>
2025-07-28 10:38:49 +0900

    wip

--- Runtime/UI/Core/Layout/LayoutIgnorer.cs
+++ Runtime/UI/Core/Layout/LayoutIgnorer.cs
@@ -1,7 +1,11 @@
+#nullable enable
+
 namespace UnityEngine.UI
 {
     [DisallowMultipleComponent]
     public class LayoutIgnorer : MonoBehaviour
     {
+        private void OnEnable() => LayoutRebuilder.SetDirty(this);
+        private void OnDisable() => LayoutRebuilder.SetDirty(this);
     }
 }
