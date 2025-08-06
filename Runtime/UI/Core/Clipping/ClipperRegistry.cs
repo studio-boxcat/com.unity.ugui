4a3ec469e935b8484f46b944188b4e10b500559c
James Kim (Daewon) <eodnjs2998@gmail.com>
2025-08-06 17:45:27 +0900

    fix clipper

--- Runtime/UI/Core/Clipping/ClipperRegistry.cs
+++ Runtime/UI/Core/Clipping/ClipperRegistry.cs
@@ -1,5 +1,7 @@
+// #define VERBOSE
 #nullable enable
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine.Assertions;
 
 namespace UnityEngine.UI
@@ -20,26 +22,39 @@ namespace UnityEngine.UI
 
         public static void RegisterClipper(Clipper c)
         {
+            V($"Registering clipper: {c}");
             _clippers.Add(c, null); // will be resolved on the next Cull() call. let it throw if the clipper is already registered.
             _dirtyClippers.Add(c);
         }
 
         public static void UnregisterClipper(Clipper c)
         {
+            V($"Unregistering clipper: {c}");
             var result = _clippers.Remove(c, out var clippables);
             Assert.IsTrue(result, "Clipper not registered in ClipperRegistry. Clipper should be registered before it is unregistered.");
             if (clippables is null) return; // not resolved yet. (no need to restore cull state or whatever)
+            V($"Unregistered clipper: {c}, restoring cull state for {clippables.Count} clippables: {string.Join(", ", clippables)}");
             _dirtyClippables.AddRange(clippables); // add only the clippables that were registered to this clipper.
+
+            // set clippables to null, so they will be resolved on the next Cull() call.
+            foreach (var cl in clippables)
+            {
+                Assert.IsTrue(_clippables.ContainsKey(cl),
+                    "Clippable must be registered in ClipperRegistry before it can be unregistered.");
+                _clippables[cl] = null;
+            }
         }
 
         public static void RegisterClippable(IClippable c)
         {
+            V($"Registering clippable: {c}");
             _clippables.Add(c, null); // will be resolved on the next Cull() call. let it throw if the clippable is already registered.
             _dirtyClippables.Add(c);
         }
 
         public static void UnregisterClippable(IClippable c)
         {
+            V($"Unregistering clippable: {c}");
             var result = _clippables.Remove(c, out var clipper);
 #if DEBUG
             if (result is false)
@@ -52,7 +67,7 @@ namespace UnityEngine.UI
             // set dirty to restore cull state in Cull().
             _dirtyClippables.Add(c);
 
-            // unregister from the Clipper immediately. it's the different part between Clippable and Clipper.
+            // unregister from the Clipper immediately.
             if (clipper is not null // clippable has been resolved and has a clipper.
                 && _clippers.TryGetValue(clipper, out var list) // registered
                 && list is not null) // clipper has been resolved
@@ -109,7 +124,7 @@ namespace UnityEngine.UI
                 var prevHash = 0; // Unity ensures that GetHashCode() is unique for each object and not 0.
                 foreach (var c in _dirtyClippables)
                 {
-                    var g = c.Graphic;
+                    var g = c.Graphic; // destroyed graphic is already pruned.
                     var h = g.GetHashCode();
                     if (h == prevHash) continue; // skip duplicates.
                     prevHash = h;
@@ -117,25 +132,38 @@ namespace UnityEngine.UI
                     // if the graphic is no longer managed by ClipperRegistry, restore its cull state.
                     if (_clippables.TryGetValue(c, out var orgClipper) is false)
                     {
+                        V($"Graphic {g} is not registered in ClipperRegistry, restoring cull state.");
                         RestoreCullState(g);
                         continue;
                     }
 
                     var newClipper = ActiveClipperFor(g);
-                    if (newClipper.RefEq(orgClipper)) continue; // no change in clipper.
+                    if (newClipper.RefEq(orgClipper))
+                    {
+                        V($"Graphic {g} already has the same clipper {orgClipper}, no need to update.");
+#if DEBUG
+                        if (orgClipper is not null && (_clippers.TryGetValue(orgClipper, out var l) is false || l is null || l.ContainsRef(c) is false))
+                            L.E($"[ClipperRegistry] Graphic {g} is already registered in ClipperRegistry, but not found in the clipper {orgClipper}. This is a bug.");
+#endif
+                        continue;
+                    }
 
                     if (orgClipper is not null
                         && _clippers.TryGetValue(orgClipper, out var clippablesByOrgClipper) // could be not exist if the clipper is unregistered.
                         && clippablesByOrgClipper is not null) // could be null if the clipper is re-registered.
                     {
+                        V($"Graphic {g} is reparented from clipper {orgClipper} to {newClipper}, removing from the old clipper.");
                         clippablesByOrgClipper.Remove(c);
                     }
 
                     // update the clipper for the clippable.
+                    V($"Graphic {g} is reparented to clipper {newClipper}, updating clippable.");
                     _clippables[c] = newClipper;
 
                     if (newClipper is not null)
                     {
+                        V($"Graphic {g} is now clipped by {newClipper}, updating cull state.");
+
                         newClipper.MarkNeedClip(); // will be used in PerformClipping().
 
                         // XXX: here, newClipper must be exists in the dictionary,
@@ -150,6 +178,7 @@ namespace UnityEngine.UI
                     }
                     else
                     {
+                        V($"Graphic {g} is not clipped by any clipper, restoring cull state.");
                         RestoreCullState(g); // no active clipper, restore cull state.
                     }
                 }
@@ -202,6 +231,9 @@ namespace UnityEngine.UI
             g.SetClipRect(new Rect(), validRect: false);
             g.UpdateCull(cull: false);
         }
+
+        [Conditional("VERBOSE")]
+        private static void V(string message) => L.I($"[ClipperRegistry] {message}");
 
 #if UNITY_EDITOR
         public static Clipper? GetCachedClipper(IClippable c) => _clippables.GetValueOrDefault(c);
